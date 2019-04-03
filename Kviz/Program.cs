dc7244255211b04859f090eaecd3e8be96bd675f
Jakub Šenkýř <senkyr@spstrutnov.cz>
2019-04-03 11:59:00 +0200

    Připravená kostra programu

--- Kviz/Program.cs
+++ Kviz/Program.cs
@@ -1,15 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kviz
 {
     class Program
     {
+        struct Odpoved
+        {
+            public string zneniOdpovedi;
+            public bool spravnostOdpovedi;
+        }
+
+        struct Otazka
+        {
+            public string zneniOtazky;
+            public List<Odpoved> mozneOdpovedi;
+        }
+
+        struct Vysledek
+        {
+            public string prezdivka;
+            public uint skore;
+        }
+
+        // funkce tymu (1) Landspersky + Hnyk + Korcak
+
+        // ---------------------------------------
+
+
+        // funkce tymu (2) Karas + Knizek + Jindra
+
+        // -----------------------------------
+
+
+        // funkce tymu (3) Lukas + Hepnar + Krejcar
+
+        // ------------------------------------
+
+
+        // funkce tymu (4) Gaspar + Janus + Janicek
+
+        // ------------------------------------
+
         static void Main(string[] args)
         {
+            Console.WriteLine(" V Í T E J T E   U   L A K A T O Š K V Í Z U ");
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine();
+
+            // ukol tymu Landspersky + Hnyk + Korcak
+            List<Otazka> otazky = nacistOtazky("kviz_data.txt");
+            zamichatOtazky(otazky);
+            // -------------------------------------
+
+            int pocetLosovanychOtazek = Math.Min(10, otazky.Count);
+            uint ziskaneSkore = 0;
+
+            for(int i = 0; i < pocetLosovanychOtazek; i++)
+            {
+                // ukol tymu Karas + Knizek + Jindra
+                polozitOtazku(otazky[i]);
+                nabidnoutOdpovedi(otazky[i]);
+
+                if(jeSpravnaOdpoved(ziskatOdpoved(), otazky[i]))
+                {
+                    ziskaneSkore += 1;
+                }
+                // ---------------------------------
+            }
+
+            // ukol tymu Lukas + Hepnar + Krejcar
+            List<Vysledek> vysledky = nacistVysledky("kviz_skore.txt");
+            bool umistilSe = jeDostatecneVysoke(ziskaneSkore, vysledky);
+            // ----------------------------------
+
+            // ukol tymu Gaspar + Janus + Janicek
+            if(umistilSe)
+            {
+                string prezdivka = ziskatPrezdivku();
+                zapsatDoVysledku(prezdivka, ziskaneSkore, vysledky);
+                zapsatNaDisk(vysledky);
+            }
+            // ----------------------------------
+
+            Console.WriteLine();
+            Console.WriteLine("Děkujeme za zahrání!");
+
+            Console.CursorVisible = false;
+            Console.ReadKey(true);
         }
     }
 }
