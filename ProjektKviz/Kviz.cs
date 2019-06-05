d54830482c940d76ba05a8638573cd582e6601f9
Dominik Hepnar <hepnar@spstrutnov.cz>
2019-06-05 12:56:47 +0200

    Sladění lokálních změn s masterem

--- ProjektKviz/Kviz.cs
+++ ProjektKviz/Kviz.cs
@@ -102,13 +102,30 @@ namespace ProjektKviz
         #endregion
 
         #region Funkce tymu (3) Lukas + Hepnar + Krejcar
-        public static List<Vysledek> NacistVysledky(string cestaSouboru)
+
+		public static List<Vysledek> NacistVysledky(string jmenoSouboruSVysledky)
         {
-            throw new NotImplementedException();
+            nacistVysledky = Console.ReadLine(jmenoSouboruSVysledky);
+
+
+
+
         }
+
         public static bool JeDostatecneVysoke(uint skore, List<Vysledek> vysledky)
         {
-            throw new NotImplementedException();
+            int x;
+            x = vysledky.Count;
+
+            int minSkoreVTabulceVysledku = (int)vysledky[x - 1].skore;
+
+            vysledky.ConsoleReadline
+
+
+            if minSkoreVTabulceVysledku < skore do
+
+
+
         }
         #endregion
 
